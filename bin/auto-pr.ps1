df3da153dcce05f3135e750bfb566789564627ce
r0x5a <155115880+r0x5a@users.noreply.github.com>
2024-11-23 18:35:11 +0800

    Update repo info

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "r0x5a/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
